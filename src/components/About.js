cc7b957930b422ebfe687ae7923d62f973d1c87e
divinedryfruitsandfoods <rushikeshkarkhanis@Rushikeshs-MacBook-Pro.local>
2022-12-31 16:18:06 +0530

    ABout us

--- src/components/About.js
+++ src/components/About.js
@@ -3,8 +3,8 @@ import React from 'react'
 export default function About() {
     return (
         <>
+            <div>This is for merge conflict</>
             <div>About US is pending</div>
         </>
-
     )
 }
