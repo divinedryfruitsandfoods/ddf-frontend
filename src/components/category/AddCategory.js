d6d1f30bb111d6bcb0bee6af09184ffd4f8f3136
Divine Dry Fruits And Foods <divinedryfruitsofficial@gmail.com>
2023-01-03 21:37:39 +0530

    product detail page completed

--- src/components/category/AddCategory.js
+++ src/components/category/AddCategory.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import Popup from "react-animated-popup";
 import { useNavigate } from "react-router-dom";
-import CategoryService from "../../services/CategoryService"
-const addCategoryForm = "p-6 flex flex-col justify-center";
-const addCategoryName = "w-100 mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-indigo-500 focus:outline-none";
-const addCategorySubmit = "md:w-32 bg-indigo-600 hover:bg-blue-dark text-white font-bold py-3 px-6 rounded-lg mt-3 hover:bg-indigo-500 transition ease-in-out duration-300";
+import CategoryService from "../../services/CategoryService";
+const addCategoryForm = "flex flex-col text-center";
+const addCategoryName = "text-white text-xs w-30 mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-indigo-500 focus:outline-none";
+const addCategorySubmit =
+  " mr-2 rounded-lg text-xs px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white";
 const addCategoryNameDiv = "flex flex-col";
 const addSubCategoryNameDiv = "flex flex-col ";
 
@@ -22,40 +23,34 @@ export default function AddCategory({ visible, setVisible }) {
 
   const saveCategory = (e) => {
     e.preventDefault();
-    CategoryService.saveCategory(category).then((response) => {
-      console.log(response);
-      navigate("/products");
-    })
+    CategoryService.saveCategory(category)
+      .then((response) => {
+        console.log(response);
+        navigate("/products");
+      })
       .catch((error) => {
         console.log(error);
       });
   };
   return (
     <>
-      <Popup visible={visible} onClose={() => setVisible(false)}>
+      <Popup visible={visible} onClose={() => setVisible(false)} className="addCategoryPopup">
         <form className={addCategoryForm}>
+          <h1>Add New Category</h1>
           <div className={addCategoryNameDiv}>
-            <input type="name" name="categoryName" id="categoryName"
-              value={category.categoryName}
-              onChange={(e) => handleChange(e)}
-              placeholder="Enter Category Name" className={addCategoryName} />
+            <input type="name" name="categoryName" id="categoryName" value={category.categoryName} onChange={(e) => handleChange(e)} placeholder="Enter Category Name" className={addCategoryName} />
           </div>
           <div className={addSubCategoryNameDiv}>
-            <input type="name" name="subCategoryName" id="subCategoryName"
-              value={category.subCategoryName}
-              onChange={(e) => handleChange(e)}
-              placeholder="Enter Sub Category Name" className={addCategoryName} />
+            <input type="name" name="subCategoryName" id="subCategoryName" value={category.subCategoryName} onChange={(e) => handleChange(e)} placeholder="Enter Sub Category Name" className={addCategoryName} />
           </div>
-          <button type="submit"
-            onClick={saveCategory}
-            className={addCategorySubmit}>
-            Add
-          </button>
-          <button type="button"
-            onClick={() => setVisible(false)}
-            className={addCategorySubmit}>
-            Close
-          </button>
+          <div className="mt-3 inline-flex shadow-sm px-6" role="grpup">
+            <button type="submit" onClick={saveCategory} className={addCategorySubmit}>
+              Add
+            </button>
+            <button type="button" onClick={() => setVisible(false)} className={addCategorySubmit}>
+              Close
+            </button>
+          </div>
         </form>
       </Popup>
     </>
--- src/components/category/UpdateCategory.js
+++ src/components/category/UpdateCategory.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { FcCancel } from "react-icons/fc";
-import { AiFillEdit } from "react-icons/ai"
+import { AiFillEdit } from "react-icons/ai";
 import { useNavigate, useParams } from "react-router-dom";
 import CategoryService from "../../services/CategoryService";
-const updateCategoryForm = "p-6 flex flex-col justify-center";
-const updateCategoryName = "w-100 mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-indigo-500 focus:outline-none";
+const updateCategoryForm = "flex flex-col justify-center";
+const updateCategoryName = "w-32 mt-2 py-2 px-2 text-white text-xs rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-indigo-500 focus:outline-none";
 const updateCategorySubmit = "md:w-32 bg-indigo-600 hover:bg-blue-dark text-white font-bold py-3 px-6 rounded-lg mt-3 hover:bg-indigo-500 transition ease-in-out duration-300";
 const updateCategoryNameDiv = "flex flex-col fit-content w-0.10";
 const updateSubCategoryNameDiv = "flex flex-col ";
@@ -49,27 +49,16 @@ export default function UpdateCategory({ visible, setVisible }) {
     <>
       <form className={updateCategoryForm}>
         <div className={updateCategoryNameDiv}>
-          <input type="name" name="categoryName" id="categoryName"
-            value={category.categoryName}
-            onChange={(e) => handleChange(e)}
-            placeholder="Enter Category Name" className={updateCategoryName} />
+          <input type="name" name="categoryName" id="categoryName" value={category.categoryName} onChange={(e) => handleChange(e)} placeholder="Enter Category Name" className={updateCategoryName} />
         </div>
         <div className={updateSubCategoryNameDiv}>
-          <input type="name" name="subCategoryName" id="subCategoryName"
-            value={category.subCategoryName}
-            onChange={(e) => handleChange(e)}
-            placeholder="Enter Sub Category Name" className={updateCategoryName} />
+          <input type="name" name="subCategoryName" id="subCategoryName" value={category.subCategoryName} onChange={(e) => handleChange(e)} placeholder="Enter Sub Category Name" className={updateCategoryName} />
         </div>
         <div className="grid grid-cols-12 gap-12">
-          <button type="submit"
-            className="inline-block"
-            onClick={updateCategory}>
+          <button type="submit" className="inline-block" onClick={updateCategory}>
             <AiFillEdit />
           </button>
-          <button
-            type="button"
-            onClick={() => navigate("/products")}
-            className=" inline-block">
+          <button type="button" onClick={() => navigate("/products")} className=" inline-block">
             <FcCancel />
           </button>
         </div>
